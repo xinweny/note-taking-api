baba32b82814af406a29de43995a36bd64189c6a
Xin Wen Yap <yapxinwen1608@gmail.com>
2026-01-02 13:07:38 +0800

    add dotenv config

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import { app } from './app.js';
 
-const port = process.env.PORT || 3000;
+import { port } from './config/env.js';
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
