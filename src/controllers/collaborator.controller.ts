6686d6a5fc772a44396e95755b86c33db5412fcd
Xin Wen Yap <yapxinwen1608@gmail.com>
2026-01-04 22:54:14 +0800

    add collaborator endpoints

--- src/controllers/collaborator.controller.ts
+++ src/controllers/collaborator.controller.ts
@@ -2,12 +2,40 @@ import { type Request, type Response } from 'express';
 
 import { Collaborator } from '../models/index.ts';
 
+// Get all collaborators of a note
 export async function getNoteCollaborators(req: Request, res: Response) {
-  const versions = await Collaborator.findAll({
+  const collaborators = await Collaborator.findAll({
     where: { noteId: +req.params.noteId! }
   });
 
   return res.status(200).json({
-    data: versions,
+    data: collaborators,
+  });
+}
+
+// Update collaborator permissions
+export async function updateCollaborator(req: Request, res: Response) {
+  const canEdit = !!req.body.canEdit;
+
+  await Collaborator.update(
+    { canEdit },
+    {
+      where: { id: +req.params.collaboratorId! }
+    }
+  );
+
+  return res.status(200).json({
+    message: 'Collaborator permissions updated successfully.',
+  });
+}
+
+// Delete collaborator from note
+export async function deleteCollaborator(req: Request, res: Response) {
+  await Collaborator.destroy({
+    where: { id: +req.params.collaboratorId! },
+  });
+
+  return res.status(200).json({
+    message: 'Collaborator deleted successfully.'
   });
 }
--- src/routes/collaborator.route.ts
+++ src/routes/collaborator.route.ts
@@ -1,3 +1,18 @@
 import { Router } from 'express';
 
+import { authorize } from '../middlewares/authorize.middleware.ts';
+
+import {
+  getNoteCollaborators,
+  updateCollaborator,
+  deleteCollaborator,
+} from '../controllers/collaborator.controller.ts';
+
 export const collaboratorRouter = Router();
+
+collaboratorRouter.use(authorize());
+collaboratorRouter.get('/', getNoteCollaborators);
+
+collaboratorRouter.use(authorize('isAdmin'));
+collaboratorRouter.put('/:collaboratorId', updateCollaborator);
+collaboratorRouter.delete('/:collaboratorId', updateCollaborator);
