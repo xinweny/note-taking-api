ca004c7aa7f7f8961841408341024365740c6be2
Xin Wen Yap <yapxinwen1608@gmail.com>
2026-01-05 11:44:03 +0800

    add cors

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 import 'dotenv/config';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 import { router } from './routes/index.ts';
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); // Use qs library when true; Use querystring library when false
