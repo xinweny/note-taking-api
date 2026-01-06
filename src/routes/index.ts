eb91af55a49644763e7c7fb03c8fcb10e9d716e4
Xin Wen Yap <yapxinwen1608@gmail.com>
2026-01-06 13:36:26 +0800

    add swagger

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,10 +4,16 @@ import { authenticate } from '../middlewares/authenticate.middleware.ts';
 
 import { authRouter } from './auth.route.ts';
 import { noteRouter } from './note.route.ts';
+import { collaboratorRouter } from './collaborator.route.ts';
+import { versionRouter } from './version.route.ts';
+import { attachmentRouter } from './attachment.route.ts';
 
 export const router = Router();
 
 router.use('/auth', authRouter);
 
-// Authenticate note routes
+// Authenticated routes
 router.use('/notes', [authenticate, noteRouter]);
+router.use('/collaborators', [authenticate, collaboratorRouter]);
+router.use('/versions', [authenticate, versionRouter]);
+router.use('/attachments', [authenticate, attachmentRouter]);
--- src/routes/version.route.ts
+++ src/routes/version.route.ts
@@ -2,8 +2,13 @@ import { Router } from 'express';
 
 import { authorize } from '../middlewares/authorize.middleware.ts';
 
-import { getNoteVersions } from '../controllers/version.controller.ts';
+import {
+  getNoteVersions,
+  getVersionById,
+} from '../controllers/version.controller.ts';
 
 export const versionRouter = Router();
 
 versionRouter.get('/', [authorize(), getNoteVersions]);
+
+versionRouter.get('/:versionId', [authorize(), getVersionById]);
